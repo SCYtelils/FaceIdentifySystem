00b76c3b85f03ec0f33934f1c3d76252990adf84
telils <telils@163.com>
2020-01-05 13:34:24 +0800

    完成了第一遍图片录入处理的代码

--- OtherPicCatch.py
+++ OtherPicCatch.py
@@ -3,8 +3,6 @@ import sys,os,cv2
 input_path='./faceSource'
 output_dir='other_faces'
 
-size=64
-
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -18,4 +16,22 @@ for (path,dirnames,filenames) in os.walk(input_path):
         if filename.endswith('.jpg'):
             print('being processed picture %s'% str(index))
             img_path=path+'/'+filename
-            
+        
+        # 从文件中读取图片
+        img=cv2.imread(img_path)
+        # 转为灰度图片
+        gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # 使用haar进行人脸检测
+        faces=haar.detectMultiScale(gray_img,1.3,5)
+        for f_x,f_y,f_w,f_h in faces:
+            face = img[f_y:f_y+f_h,f_x:f_x+f_w]
+            # 统一保存为64*64格式
+            face = cv2.resize(face,(64,64))
+            cv2.imshow('img',face)
+            cv2.imwrite(output_dir+'/'+str(index)+'.jpg',face)
+            index = index + 1
+        if index == 10000:
+            sys.exit(0)
+            key=cv2.waitKey(30)&0xff
+            if key == 27:
+                sys.exit(0)
